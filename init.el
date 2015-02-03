dbb6c0f70645e9c06e1911560d0ff84c08f1f2d3
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-02-03 10:10:17 +0800

    init.el: update
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- init.el
+++ init.el
@@ -2,20 +2,24 @@
 (eval-when-compile (require 'cl))
 
 ;; Define emacs-dir where all the files live.
-;; We set `user-emacs-directory' here so we can use command-line
-;; switch different emacs configuration like following:
-;;
-;;    emacs -q -l ~/coldnew-spacemacs/init.el
-;;
+(defconst emacs-dir
+  (file-name-directory (or load-file-name (buffer-file-name))))
+
+;; Define spacemacs private layer path (Note the backslash at end.)
+(defconst my/private-layer (concat emacs-dir "private/"))
 
 ;; Bind init.el to spacemacs, so we can make a entry point to
 ;; test spacemacs without change original emacs config.
-(let* ((emacs-dir
-        (file-name-directory (or load-file-name (buffer-file-name))))
-       (spacemacs-init
+;; We also use config.el replace spacemacs's .spacemacs file.
+(let* ((spacemacs-init
         (concat emacs-dir "spacemacs/init.el")))
 
   ;; change user-emacs-directory
+  ;; We set `user-emacs-directory' here so we can use command-line
+  ;; switch different emacs configuration like following:
+  ;;
+  ;;    emacs -q -l ~/coldnew-spacemacs/init.el
+  ;;
   (setq user-emacs-directory (file-name-directory spacemacs-init))
 
   ;; load my config
