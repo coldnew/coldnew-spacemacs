0b7798cc2eee18e33aa13d82dd4df5e5d742eab3
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-02-04 09:25:17 +0800

    Add f on coldnew-core
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- private/coldnew-core/packages.el
+++ private/coldnew-core/packages.el
@@ -23,6 +23,7 @@
     rainbow-mode
     linum-off
     flx-ido
+    f
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
