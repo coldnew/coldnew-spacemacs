6790fe221aa866b5966d99c2c96d4c03b70fdf72
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-02-25 22:57:00 +0800

    clean spacemacs config
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- spacemacs.el
+++ spacemacs.el
@@ -99,7 +99,6 @@
 (defun dotspacemacs/config ()
   "This is were you can ultimately override default Spacemacs configuration.
 This function is called at the very end of Spacemacs initialization."
-  (setq powerline-default-separator 'arrow)
   )
 
 ;; Custom variables
